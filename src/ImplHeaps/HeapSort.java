699c0f04a9aac4df64c74f03e7b7927b29d0e24a
Gero22 <38483690+Gero22@users.noreply.github.com>
2020-06-22 17:25:21 -0300

    heapSort

---
+++ src/ImplHeaps/HeapSort.java
@@ -0,0 +1,62 @@
+package MisImplementaciones.Estaticas;
+
+import java.util.Arrays;
+
+public class HeapSort {
+	
+    public void sort(int arrA[]) {
+        int tam = arrA.length; // Tamaño del arreglo.
+        int posPadre = (tam / 2) - 1; // Pos del ultimo padre.
+
+        // Para construir un monticulo.
+        for(int i = posPadre; i >= 0; i--) 
+            heapify(arrA, tam, i);
+
+        // Uno por una saco el maximo elemento del arreglo del heap y
+        // lo reemplazo con el ultimo elemento del arreglo.
+        for (int i=tam-1; i>=0; i--) {
+
+            //arrA[0] es la raiz del heap y el maximo elemento en el arreglo.
+            int x = arrA[0];
+            arrA[0] = arrA[i];
+            arrA[i] = x;
+
+            heapify(arrA, i, 0);
+        }
+    }
+
+    void heapify(int arrA[], int tamHeap, int i) {
+        int mayor = i; // Inicializar mayor como root.
+        int posIzq  = 2*i + 1; // Posicion del hijo izquierdo.
+        int posDer  = 2*i + 2; // Posicion del hijo derecho.
+
+        // Si el hijo izquiero es mas grande que la raiz.
+        if (posIzq  < tamHeap && arrA[posIzq ] > arrA[mayor])
+            mayor = posIzq ;
+
+        // Si el hijo derecho es mas grande del elemento mayor hasta el momento.
+        if (posDer  < tamHeap && arrA[posDer ] > arrA[mayor])
+            mayor = posDer ;
+
+        // Si mayor no es la raiz.
+        if(mayor != i) {
+            int aux = arrA[i];
+            arrA[i] = arrA[mayor];
+            arrA[mayor] = aux;
+
+            // Llama recursiva para el sub-arbol.
+            heapify(arrA, tamHeap, mayor);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arrA[] = {5, 8, 6, 1, 4, 7, 3, 9, 2, 0};
+
+        System.out.println("Arreglo original: " + Arrays.toString(arrA));
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(arrA);
+
+        System.out.println("Arreglo ordenando con Heap Sort: " + Arrays.toString(arrA));
+    }
+
+}
